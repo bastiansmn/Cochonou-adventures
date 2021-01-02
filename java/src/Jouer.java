c8eb9bc047c750282604bbc04571efc1e3316b90
bastiansmn <bastian.somon@gmail.com>
2021-01-02 15:54:08 +0100

    Fix bugs

--- java/src/Jouer.java
+++ java/src/Jouer.java
@@ -5,12 +5,9 @@ import java.lang.reflect.InvocationTargetException;
 
 public class Jouer {
    public static void main(String[] args) throws InterruptedException, InvocationTargetException {
-      args = new String [2];
-      args[0] = "Dylan";
-      args[1] = "-g";
-      if (args.length == 2) {
+      if (args.length == 2)
          System.out.println(new Jeu(args).run());
-      } else
+      else
          vue.terminal.Launcher.displayHelp();
    }
 }
--- java/src/modele/jeu/grille/blocs/BlocBombe.java
+++ java/src/modele/jeu/grille/blocs/BlocBombe.java
@@ -6,51 +6,63 @@ public class BlocBombe extends Bloc implements Deplacable, Ouvrable {
    @Override
    public int open(Grille g, int i, int j) {
       int summ = 0;
-      try {
-         if (g.getCases()[i][j+1].getContent() instanceof Ouvrable) {
-            g.viderCase(i, j+1);
-            summ++;
-         }
-      } catch (NullPointerException | ArrayIndexOutOfBoundsException ignored) {}
-      try {
-         if (g.getCases()[i][j-1].getContent() instanceof Ouvrable) {
-            g.viderCase(i, j-1);
-            summ++;
-         }
-      } catch (NullPointerException | ArrayIndexOutOfBoundsException ignored) {}
-      try {
-         if (g.getCases()[i-1][j-1].getContent() instanceof Ouvrable) {  g.viderCase(i-1, j-1);
-            summ++;
-         }
-      } catch (NullPointerException | ArrayIndexOutOfBoundsException ignored) {}
-      try {
-         if (g.getCases()[i-1][j].getContent() instanceof Ouvrable) {  g.viderCase(i-1, j);
-            summ++;
-         }
-      } catch (NullPointerException | ArrayIndexOutOfBoundsException ignored) {}
-      try {
-         if (g.getCases()[i-1][j+1].getContent() instanceof Ouvrable) {
-            g.viderCase(i-1, j+1);
-            summ++;
-         }
-      } catch (NullPointerException | ArrayIndexOutOfBoundsException ignored) {}
-      try {
-         if (g.getCases()[i+1][j-1].getContent() instanceof Ouvrable) {  g.viderCase(i+1, j-1);
-            summ++;
-         }
-      } catch (NullPointerException | ArrayIndexOutOfBoundsException ignored) {}
-      try {
-         if (g.getCases()[i+1][j].getContent() instanceof Ouvrable) {  g.viderCase(i+1, j);
-            summ++;
-         }
-      } catch (NullPointerException | ArrayIndexOutOfBoundsException ignored) {}
-      try {
-         if (g.getCases()[i+1][j+1].getContent() instanceof Ouvrable) {  g.viderCase(i+1, j+1);
-            summ++;
-         }
-      } catch (NullPointerException | ArrayIndexOutOfBoundsException ignored) {}
-
       g.viderCase(i, j);
+      try {
+         if (g.getCases()[i][j+1].getContent() instanceof BlocBombe)
+            ((BlocBombe) g.getCases()[i][j+1].getContent()).open(g, i, j+1);
+         else if (g.getCases()[i][j+1].getContent() instanceof Ouvrable)
+            g.viderCase(i, j+1);
+         summ++;
+      } catch (NullPointerException | ArrayIndexOutOfBoundsException ignored) {}
+      try {
+         if (g.getCases()[i][j-1].getContent() instanceof BlocBombe)
+            ((BlocBombe) g.getCases()[i][j-1].getContent()).open(g, i, j-1);
+         else if (g.getCases()[i][j-1].getContent() instanceof Ouvrable)
+            g.viderCase(i, j-1);
+         summ++;
+      } catch (NullPointerException | ArrayIndexOutOfBoundsException ignored) {}
+      try {
+         if (g.getCases()[i-1][j-1].getContent() instanceof BlocBombe)
+            ((BlocBombe) g.getCases()[i-1][j-1].getContent()).open(g, i-1, j-1);
+         else if (g.getCases()[i-1][j-1].getContent() instanceof Ouvrable)
+            g.viderCase(i-1, j-1);
+         summ++;
+      } catch (NullPointerException | ArrayIndexOutOfBoundsException ignored) {}
+      try {
+         if (g.getCases()[i-1][j].getContent() instanceof BlocBombe)
+            ((BlocBombe) g.getCases()[i-1][j].getContent()).open(g, i-1, j);
+         else if (g.getCases()[i-1][j].getContent() instanceof Ouvrable)
+            g.viderCase(i-1, j);
+         summ++;
+      } catch (NullPointerException | ArrayIndexOutOfBoundsException ignored) {}
+      try {
+         if (g.getCases()[i-1][j+1].getContent() instanceof BlocBombe)
+            ((BlocBombe) g.getCases()[i-1][j+1].getContent()).open(g, i-1, j+1);
+         else if (g.getCases()[i-1][j+1].getContent() instanceof Ouvrable)
+            g.viderCase(i-1, j+1);
+         summ++;
+      } catch (NullPointerException | ArrayIndexOutOfBoundsException ignored) {}
+      try {
+         if (g.getCases()[i+1][j-1].getContent() instanceof BlocBombe)
+            ((BlocBombe) g.getCases()[i+1][j-1].getContent()).open(g, i+1, j-1);
+         else if (g.getCases()[i+1][j-1].getContent() instanceof Ouvrable)
+            g.viderCase(i+1, j-1);
+         summ++;
+      } catch (NullPointerException | ArrayIndexOutOfBoundsException ignored) {}
+      try {
+         if (g.getCases()[i+1][j].getContent() instanceof BlocBombe)
+            ((BlocBombe) g.getCases()[i+1][j].getContent()).open(g, i+1, j);
+         else if (g.getCases()[i+1][j].getContent() instanceof Ouvrable)
+            g.viderCase(i+1, j);
+         summ++;
+      } catch (NullPointerException | ArrayIndexOutOfBoundsException ignored) {}
+      try {
+         if (g.getCases()[i+1][j+1].getContent() instanceof BlocBombe)
+            ((BlocBombe) g.getCases()[i+1][j+1].getContent()).open(g, i+1, j+1);
+         else if (g.getCases()[i+1][j+1].getContent() instanceof Ouvrable)
+            g.viderCase(i+1, j+1);
+         summ++;
+      } catch (NullPointerException | ArrayIndexOutOfBoundsException ignored) {}
 
       return summ;
    }
