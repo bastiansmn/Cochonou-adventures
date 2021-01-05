a82e9de35ddc6b649fe81f01957cf98a4750e265
bastiansmn <bastian.somon@gmail.com>
2021-01-05 21:11:11 +0100

    Finitions du code

--- java/src/vue/graphique/Fenetre.java
+++ java/src/vue/graphique/Fenetre.java
@@ -7,7 +7,6 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import modele.jeu.Jeu;
-
 public class Fenetre extends JFrame {
     CardLayout cl;
     JPanel general = new JPanel();
--- java/src/vue/graphique/Menu.java
+++ java/src/vue/graphique/Menu.java
@@ -12,8 +12,8 @@ import java.util.ArrayList;
 
 public class Menu extends vue.graphique.ImagePanel implements ActionListener {
     Fenetre fenetre;
-    JLabel[] etiquettes = new JLabel[10];
-    JButton precedent, suivant;
+    JLabel[] etiquettes;
+    JButton precedent, suivant, reglements;
     ArrayList<JButton> boutons = new ArrayList<>();
     Grille g = modele.jeu.Jeu.plateau.getNiveaux().get(0).getGrille();
     JPanel partie = new vue.graphique.Partie(fenetre, g);
@@ -35,20 +35,25 @@ public class Menu extends vue.graphique.ImagePanel implements ActionListener {
         Niveau(int i, int x, int y) {
             niveau = i + 1;
             etiquettes[index] = new JLabel("  NIVEAU " + niveau);
-            if(Jeu.plateau.getNiveaux().get(i).getGrille().isGagne()) {
+            if(Jeu.plateau.getNiveaux().get(i).canPlay()) {
                 etiquettes[index].setForeground(new Color(0, 0, 0));
                 this.setBackground(new Color(243, 202, 32));
+                if(Jeu.plateau.getNiveaux().get(i).getGrille().isGagne()) {
+                    this.setToolTipText("Score : " + Jeu.joueur.getScore());
+                }
             } else {
                 etiquettes[index].setForeground(new Color(243, 202, 32));
                 this.setBackground(new Color(0, 0, 0));
             }
             this.setBounds(x, y, 100, 40);
+            this.setText("" + i);
             this.add(etiquettes[index]);
             index++;
         }
     }
 
     void creationBoutons(int niveauActuel, int niveauMax) {
+        this.removeAll();
         int max = 10;
         if ((niveauActuel - niveauActuel % 10) != 0) {
             precedent = new JButton("Précedent");
@@ -73,8 +78,15 @@ public class Menu extends vue.graphique.ImagePanel implements ActionListener {
         for (int i = 0; i < max; i++) {
             boutons.add(new Niveau((niveauActuel - niveauActuel % 10) + i, coords[i][0], coords[i][1]));
             boutons.get(i).addActionListener(this);
-            this.add(boutons.get(i));
+            boutons.get(i).setBorderPainted(false);
+            this.add(boutons.get(i), "i");
         }
+        reglements = new JButton("Règles du jeu");
+        reglements.setBounds(825, 20, 120, 40);
+        reglements.setBackground(new Color(217, 2, 238));
+        reglements.setForeground(new Color(0, 0, 0));
+        reglements.addActionListener(this);
+        this.add(reglements);
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -89,14 +101,32 @@ public class Menu extends vue.graphique.ImagePanel implements ActionListener {
             JPanel menuBis = new Menu(fenetre, Niveau = Niveau + 10, 100);
             fenetre.general.add(menuBis, "MenuBis");
             fenetre.cl.show(fenetre.general, "MenuBis");
-        } else {
-            Niveau = (Niveau - Niveau % 10) + boutons.indexOf(source);
-            fenetre.remove(partie);
-            Grille g = modele.jeu.Jeu.plateau.getNiveaux().get(Niveau).getGrille();
-            partie = new vue.graphique.Partie(fenetre, g);
-            fenetre.general.add(partie, "Partie");
-            fenetre.cl.show(fenetre.general, "Partie");
-            fenetre.validate();
+        } else if (source == reglements) {
+            JOptionPane.showMessageDialog(fenetre,
+                    "<html>But du jeu : </br>" +
+                    "    - Dans chaque niveau, sauvez tous les animaux.<br/>" +
+                    "    - Cassez les blocs pour les faire descendre jusqu'en bas.<br/>" +
+                    "    - Utilisez vos bonus lorsque vous êtes coincés.<br/>" +
+                    "<br>" +
+                    "Comment jouer : <br/>" +
+                    "    - Cliquez ou entrez la position du bloc que vous souhaitez casser, ses blocs adjacents de même</html>",
+                    "Regles du jeu",
+                    JOptionPane.QUESTION_MESSAGE);
+        }
+        else {
+            if(Jeu.plateau.getNiveaux().get(Integer.parseInt(((JButton)e.getSource()).getText())).canPlay()) {
+                Niveau = (Niveau - Niveau % 10) + boutons.indexOf(source);
+                fenetre.remove(partie);
+                partie = new vue.graphique.Partie(fenetre, modele.jeu.Jeu.plateau.getNiveaux().get(Niveau).getGrille());
+                fenetre.general.add(partie, "Partie");
+                fenetre.cl.show(fenetre.general, "Partie");
+                fenetre.validate();
+            } else {
+                JOptionPane.showMessageDialog(fenetre,
+                        "Désolé, vous ne pouvez pas encore accéder à ce niveau.",
+                        "Niveau inaccessible",
+                        JOptionPane.ERROR_MESSAGE);
+            }
         }
     }
 }
